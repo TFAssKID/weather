0467edd4b3c39a431bf2f5345c4454ce926a8f97
Alejandro Roble Gutiérrez <arobleg@gmail.com>
2018-08-31 18:20:29 -0400

    Reimplemen checkWeather to send a request and catch exception

--- modules/custom/weather/src/Controller/WeatherController.php
+++ modules/custom/weather/src/Controller/WeatherController.php
@@ -1,15 +1,30 @@
 <?php
 namespace Drupal\weather\Controller;
 use Drupal\Core\Controller\ControllerBase;
+use GuzzleHttp\Exception\ClientException;
 
 class WeatherController extends ControllerBase {
 
     public function checkWeather()
     {
-        //$response= drupal_http_request('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=f7f726dac4af70b5f506886e2f8c7c70');
+        try {
 
-        return array(
-            '#markup' => hello_hello_world(),
-        );
+
+            $client = \Drupal::httpClient();
+            $request = $client->get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=f7f726dac4af70b5f506886e2f8c7c70');
+            $response = json_decode($request->getBody());
+
+            return array(
+                '#markup' => "FAIL",
+            );
+        }
+        catch (ClientException $e) {
+
+            return array(
+                '#markup' => $e->getMessage(),
+            );
+
+        }
+
     }
 }
