86b273acfa2928fe7f5c8f7a4ed9b9af5312c0d4
Alejandro Roble Gutiérrez <arobleg@gmail.com>
2018-08-31 21:05:50 -0400

    Decode json response and make a table with interest values

--- modules/custom/weather/src/Controller/WeatherController.php
+++ modules/custom/weather/src/Controller/WeatherController.php
@@ -12,10 +12,10 @@ class WeatherController extends ControllerBase {
 
             $client = \Drupal::httpClient();
             $request = $client->get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=f7f726dac4af70b5f506886e2f8c7c70');
-            $response = json_decode($request->getBody());
+            $response = json_decode($request->getBody(), true);
 
             return array(
-                '#markup' => "FAIL",
+                '#markup' => $this->renderTable($response),
             );
         }
         catch (ClientException $e) {
@@ -26,5 +26,34 @@ class WeatherController extends ControllerBase {
 
         }
 
+    }
+
+    public function renderTable(array $resp)
+    {
+        $table = <<< LINCOLN
+        <h1>{$resp['name']}</h1>
+        <table>
+                <tbody>
+                    <tr>
+                        <th>Features</th>
+                        <th>Value</th>
+                    </tr>
+                    <tr>
+                        <td>Temp</td>
+                        <td> {$resp['main']['temp']} </td>
+                    </tr>
+                    <tr>
+                        <td>Pressure</td>
+                        <td> {$resp['main']['pressure']} </td>
+                    </tr>
+                    <tr>
+                        <td>Humidity</td>
+                        <td> {$resp['main']['humidity']} </td>
+                    </tr>
+	            </tbody>
+               </table>
+LINCOLN;
+
+        return $table;
     }
 }
