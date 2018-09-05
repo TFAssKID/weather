df1ccdf1fb5b8ade285012e2aa2231dc2f8f2482
Alejandro Roble Gutiérrez <arobleg@gmail.com>
2018-09-05 11:35:30 -0400

    Implement funtions for load data and insert data

--- modules/custom/weather/src/Form/WeatherForm.php
+++ modules/custom/weather/src/Form/WeatherForm.php
@@ -9,15 +9,17 @@ class WeatherForm extends FormBase {
 
     public function buildForm(array $form, FormStateInterface $form_state) {
 
+        $connection = \Drupal::database();
+
+        $this->generateInformation($connection);
+
+        $options = $this->loadOptions($connection);
+
         // Select.
         $form['favorite'] = [
             '#type' => 'select',
             '#title' => $this->t('City to check'),
-            '#options' => [
-                'Bogota,co' => $this->t('Bogota'),
-                'London,uk' => $this->t('London'),
-                'Miami,us' => $this->t('Miami'),
-            ],
+            '#options' => $options,
         ];
 
         $form['actions'] = [
@@ -47,6 +49,56 @@ class WeatherForm extends FormBase {
         */
         $job_title = $form_state->getValue('job_title');
         drupal_set_message(t('You specified a job title of %job_title.', ['%job_title' => $job_title]));
+    }
+
+    private function loadOptions(&$connection)
+    {
+        $result = $connection->query("SELECT * FROM {city}");
+
+        $options = array();
+
+        foreach ($result as $record) {
+            $options[$record->city_name.','.$record->country_code] = $record->city_name;
+        }
+
+        return $options;
+    }
+
+    private function generateInformation(&$connection)
+    {
+        $result = $connection->query("SELECT * FROM {city}");
+        $records = $result->fetchAll();
+        $num_results = count($records);
+
+        if($num_results === 0)
+        {
+            $values = [
+                [
+                    'city_name' => 'Bogota',
+                    'country_code' => 'co',
+                ],
+                [
+                    'city_name' => 'London',
+                    'country_code' => 'uk',
+                ],
+                [
+                    'city_name' => 'Cali',
+                    'country_code' => 'co',
+                ],
+                [
+                    'city_name' => 'Miami',
+                    'country_code' => 'us',
+                ],
+            ];
+
+            $query = $connection->insert('city')->fields(['city_name', 'country_code']);
+
+            foreach ($values as $record) {
+                $query->values($record);
+            }
+
+            $query->execute();
+        }
     }
     
 }
